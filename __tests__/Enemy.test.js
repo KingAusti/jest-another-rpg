c4d42e70fc1c3eea9bc7ee7cea3930f4429f55c1
KingAusti <austin@thehenrys.xyz>
2022-03-30 22:00:30 -0700

    created enemy constructor

---
+++ __tests__/Enemy.test.js
@@ -0,0 +1,28 @@
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
+
+jest.mock('../lib/Potion.js');
+
+test('creates an enemy object', () => {
+  const enemy = new Enemy('goblin', 'sword');
+
+  expect(enemy.name).toBe('goblin');
+  expect(enemy.weapon).toBe('sword');
+  expect(enemy.health).toEqual(expect.any(Number));
+  expect(enemy.strength).toEqual(expect.any(Number));
+  expect(enemy.agility).toEqual(expect.any(Number));
+  expect(enemy.potion).toEqual(expect.any(Object));
+});
+
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+  });
+  
+  test('gets a description of the enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+  
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+  });
